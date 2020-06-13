849ec8f578b9baa1dd1440870680167c58327963
mohuneko <b0wdmohu@gmail.com>
2020-06-13 08:18:13 +0000

    model編集

--- app/models/abc.rb
+++ app/models/abc.rb
@@ -1,4 +1,4 @@
-class Admin < ApplicationRecord
+class Abc < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
