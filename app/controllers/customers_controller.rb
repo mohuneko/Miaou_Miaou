418cd47814c8423d1165a4eab0932bed12e7f235
mohuneko <b0wdmohu@gmail.com>
2020-06-13 08:07:31 +0000

    usercontroller編集

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,10 +3,6 @@ class CustomersController < ApplicationController
  def mypage
  end
 
- def 	
- end
-
-
  private
  def customer_params
  	params.require(:customer).permit(:name, :password, :tell, :profile_image_id, :status)
